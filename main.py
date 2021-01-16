9b394a5fe68207b23a406623751a09cc0f8a2e7d
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-16 17:10:23 -0500

    create random word functionality

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from tkinter import *
+import pandas as pd
+from random import randint
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+df = pd.read_csv('data/mandarin_concatenated.csv')
+df_to_dict = df.to_dict(orient='records')
+# print(df_to_dict[0]['Mandarin'])
+
+
+# random mandarin word
+def random_mandarin_word():
+    mandarin_word = df_to_dict[randint(0, 5000)]['Mandarin']
+    canvas.delete('word')
+    canvas.create_text(400, 263, text=mandarin_word, font=('Arial', 60, 'bold'), tag='word')
 
 
 # Window
@@ -11,24 +25,22 @@ window.resizable(False, False)
 
 
 # Card
-canvas = Canvas(width=800, height=526, highlightthickness=0)
-bg_img = PhotoImage(file='images/card_front.png')
-canvas.create_image(400, 263, image=bg_img)
-canvas.config(background=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file='images/card_front.png')
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text='Mandarin', font=('Arial', 40, 'italic'))
+canvas.create_text(400, 263, text='word', font=('Arial', 60, 'bold'), tag='word')
+canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 
-# Copy
-
-
-
 # Buttons
-btn_wrong_img = PhotoImage(file='images/wrong.png')
-btn_wrong = Button(image=btn_wrong_img, highlightthickness=0)
+btn_crossed_img = PhotoImage(file='images/wrong.png')
+btn_wrong = Button(image=btn_crossed_img, highlightthickness=0, command=random_mandarin_word)
 btn_wrong.grid(column=0, row=1)
 
 btn_right_img = PhotoImage(file='images/right.png')
-btn_right = Button(image=btn_right_img, highlightthickness=0)
+btn_right = Button(image=btn_right_img, highlightthickness=0, command=random_mandarin_word)
 btn_right.grid(column=1, row=1)
 
 
