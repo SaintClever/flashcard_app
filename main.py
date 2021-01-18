3204a0abe48f4c235587950491035a40bc0fd0a2
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-17 22:13:41 -0500

    drastic ui update

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import pandas as pd
 from random import choice
 
 
-BACKGROUND_COLOR = "#B1DDC6"
+# unused colors: '#32373c', '#3c4146'
+BACKGROUND_COLOR = '#ffffff'
+FLIP_TIME = 5000
+
+
 current_word = {}
 df_to_dict = {}
+
 
 try:
     df = pd.read_csv('data/words_to_learn.csv')
@@ -14,7 +19,7 @@ except FileNotFoundError:
     df_to_dict = original_df.to_dict(orient='records')
 else:
     df_to_dict = df.to_dict(orient='records')
-    
+
 
 # random mandarin word
 def random_word():
@@ -25,7 +30,7 @@ def random_word():
     canvas.itemconfig(card_img, image=card_front_img)
     canvas.itemconfig(card_title, text='Mandarin', fill='#000000')
     canvas.itemconfig(card_word, text=current_word['Mandarin'], fill='#000000')
-    flip_timer = window.after(3000, func=flip_card)
+    flip_timer = window.after(FLIP_TIME, func=flip_card)
 
 
 def flip_card():
@@ -36,7 +41,6 @@ def flip_card():
 
 def is_known():
     df_to_dict.remove(current_word)
-    # print(len(df_to_dict))
     data = pd.DataFrame(df_to_dict)
     data.to_csv('data/words_to_learn.csv', index=False)
     random_word()
@@ -45,34 +49,33 @@ def is_known():
 # Window
 window = Tk()
 window.title('Flashy: flashcards')
-window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+window.config(padx=0, pady=0, background=BACKGROUND_COLOR)
 window.resizable(False, False)
 flip_timer = window.after(3000, func=flip_card)
 
+
 # Card
-canvas = Canvas(width=800, height=526)
+canvas = Canvas(width=600, height=480)
+flags = PhotoImage(file='images/flags.png')
 card_front_img = PhotoImage(file='images/card_front.png')
 card_back_img = PhotoImage(file='images/card_back.png')
-card_img = canvas.create_image(400, 263, image=card_front_img) # NOTE: name is 'card_background'
-card_title = canvas.create_text(400, 150, text='title', font=('Arial', 40, 'italic'))
-card_word = canvas.create_text(400, 263, text='word', font=('Arial', 60, 'bold'))
+flag_banner = canvas.create_image(300, 0, image=flags)
+card_img = canvas.create_image(300, 265, image=card_front_img)
+card_title = canvas.create_text(300, 165, text='title', font=('Century Gothic', 35, 'italic'), justify='center')
+card_word = canvas.create_text(300, 265, text='word', font=('Century Gothic', 45, 'normal'), justify='center')
 canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(column=0, row=0, columnspan=2)
+canvas.grid(column=0, row=1, columnspan=2)
 
 
 # Buttons
 btn_crossed_img = PhotoImage(file='images/wrong.png')
 btn_wrong = Button(image=btn_crossed_img, highlightthickness=0, command=random_word)
-btn_wrong.grid(column=0, row=1)
+btn_wrong.grid(column=0, row=2, pady=(0, 20))
 
 btn_right_img = PhotoImage(file='images/right.png')
 btn_right = Button(image=btn_right_img, highlightthickness=0, command=is_known)
-btn_right.grid(column=1, row=1)
-
+btn_right.grid(column=1, row=2, pady=(0, 20))
 
 
 random_word()
-
-
-
 window.mainloop()
